a5a9d84f503ca8eee4cc54566c7ae58bf74bece6
Minh Lu <minhlu3@gmail.com>
2025-03-17 08:16:45 -0700

    Fix both variants for LC199 and add more testcases

--- 199_binary_tree_right_side_view/python/first_variant_left_right_side_view_199.py
+++ 199_binary_tree_right_side_view/python/first_variant_left_right_side_view_199.py
@@ -1,31 +1,39 @@
-from ...utils.treenode import TreeNode
 from typing import Optional
 from collections import deque
 
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
 
 class Solution:
     def leftRightSideViewVariant(self, root: Optional[TreeNode]) -> list[int]:
-        if root is None:
+        if not root:
             return []
 
-        leftside = []
-        rightside = []
-        queue = deque([root])
+        left_side = []
+        right_side = []
+        q = deque([root])
+        while q:
+            size = len(q)
+            for i in range(size):
+                node = q.popleft()
 
-        while queue:
-            level_length = len(queue)
-            for i in range(level_length):
-                curr = queue.popleft()
                 if i == 0:
-                    leftside.append(curr.val)
-                if i == level_length - 1 and level_length != 1:
-                    rightside.append(curr.val)
-                if curr.left:
-                    queue.append(curr.left)
-                if curr.right:
-                    queue.append(curr.right)
+                    left_side.append(node.val)
+                if size == i + 1:
+                    right_side.append(node.val)
 
-        return leftside[::-1] + rightside
+                if node.left:
+                    q.append(node.left)
+                if node.right:
+                    q.append(node.right)
+
+        result = []
+        result.extend(reversed(left_side))
+        result.extend(right_side[1:])
+        return result
 
 
 if __name__ == "__main__":
@@ -56,3 +64,33 @@ if __name__ == "__main__":
     root2.left.left = TreeNode(4)
     root2.left.right = TreeNode(5)
     assert solution.leftRightSideViewVariant(root2) == [4, 2, 1, 3, 5]
+
+    root1 = TreeNode(1)
+    root1.left = TreeNode(2)
+    root1.right = TreeNode(3)
+    root1.right.right = TreeNode(7)
+    root1.right.left = TreeNode(6)
+    root1.right.right.left = TreeNode(8)
+    root1.left.left = TreeNode(4)
+    root1.left.right = TreeNode(5)
+    assert solution.leftRightSideViewVariant(root1) == [8, 4, 2, 1, 3, 7, 8]
+
+    root2 = TreeNode(1)
+    assert solution.leftRightSideViewVariant(root2) == [1]
+
+    root3 = TreeNode(1)
+    root3.left = TreeNode(2)
+    root3.left.left = TreeNode(3)
+    assert solution.leftRightSideViewVariant(root3) == [3, 2, 1, 2, 3]
+
+    root4 = None
+    assert solution.leftRightSideViewVariant(root4) == []
+
+    root5 = TreeNode(1)
+    root5.left = TreeNode(2)
+    root5.right = TreeNode(3)
+    root5.right.left = TreeNode(5)
+    root5.right.left.right = TreeNode(6)
+    root5.right.left.right.right = TreeNode(7)
+    root5.left.right = TreeNode(4)
+    assert solution.leftRightSideViewVariant(root5) == [7, 6, 4, 2, 1, 3, 5, 6, 7]
--- 199_binary_tree_right_side_view/python/second_variant_print_left_right_side_views_199.py
+++ 199_binary_tree_right_side_view/python/second_variant_print_left_right_side_views_199.py
@@ -1,34 +1,38 @@
-from ...utils.treenode import TreeNode
 from typing import Optional
 from collections import deque
 
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
 
 class Solution:
     def leftRightSideViewVariantPrint(self, root: Optional[TreeNode]):
-        if root is None:
+        if not root:
             return []
 
-        leftside = []
-        rightside = []
-        queue = deque([root])
+        left_side = []
+        right_side = []
+        q = deque([root])
+        while q:
+            size = len(q)
+            for i in range(size):
+                node = q.popleft()
 
-        while queue:
-            level_length = len(queue)
-            for i in range(level_length):
-                curr = queue.popleft()
                 if i == 0:
-                    leftside.append(curr.val)
-                if i == level_length - 1 and level_length != 1:
-                    rightside.append(curr.val)
-                if curr.left:
-                    queue.append(curr.left)
-                if curr.right:
-                    queue.append(curr.right)
+                    left_side.append(node.val)
+                if size == i + 1:
+                    right_side.append(node.val)
 
-        for left_value in leftside[::-1]:
+                if node.left:
+                    q.append(node.left)
+                if node.right:
+                    q.append(node.right)
+
+        for left_value in left_side[-1:0:-1]:
             print(left_value)
-
-        for right_value in rightside:
+        for right_value in right_side:
             print(right_value)
 
 
@@ -61,3 +65,37 @@ if __name__ == "__main__":
     root2.left.left = TreeNode(4)
     root2.left.right = TreeNode(5)
     solution.leftRightSideViewVariantPrint(root2)  # Should print 4, 2, 1, 3, 5 in order
+    print("-")
+
+    # Tree structure:
+    #       1
+    #      / \
+    #     2   3
+    #    /     \
+    #   5       4
+    root1 = TreeNode(1)
+    root1.left = TreeNode(2)
+    root1.right = TreeNode(3)
+    root1.left.left = TreeNode(5)
+    root1.right.right = TreeNode(4)
+    solution.leftRightSideViewVariantPrint(root1)  # Expected output: 5 2 1 3 4
+    print("-")
+
+    root2 = TreeNode(1)
+    solution.leftRightSideViewVariantPrint(root2)  # Expected output: 1
+    print("-")
+
+    root3 = None
+    solution.leftRightSideViewVariantPrint(root3)  # Expected output: (nothing)
+    print("-")
+
+    # Tree structure:
+    #       1
+    #      /
+    #     2
+    #    /
+    #   3
+    root4 = TreeNode(1)
+    root4.left = TreeNode(2)
+    root4.left.left = TreeNode(3)
+    solution.leftRightSideViewVariantPrint(root4)  # Expected output: 3 2 1 2 3
